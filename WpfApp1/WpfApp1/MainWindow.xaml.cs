e80dda309dd3c79722b71183beb692d781282aa5
Anthony_FU <xuanyylhky@163.com>
2018-09-04 09:21:01 +0800

    AddProject
    添加工程

---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp1.UserCtrl;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // 新建一个文本开关
+            TextToggle textToggle = new TextToggle();
+            textToggle.parent = this.MyStack;
+
+            MyStack.Children.Add(textToggle);
+        }
+    }
+}
---
+++ WpfApp1/WpfApp1/UserCtrl/TextToggle.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1.UserCtrl
+{
+    /// <summary>
+    /// UserControl1.xaml 的交互逻辑
+    /// </summary>
+    public partial class TextToggle : UserControl
+    {
+        public StackPanel parent;
+
+        private bool isOn;
+
+        public TextToggle()
+        {
+            isOn = false;
+
+            InitializeComponent();
+        }
+
+        public bool IsOn
+        {
+            get { return isOn; }
+            set
+            {
+                isOn = value;
+                if (isOn)
+                {
+                    // 点击加删除线, 显示√
+                    Toggle_Text.TextDecorations = TextDecorations.Strikethrough;
+                    this.Toggle_Image.Visibility = Visibility.Visible;
+                }
+                else
+                {
+                    Toggle_Text.TextDecorations = null;
+                    this.Toggle_Image.Visibility = Visibility.Hidden;
+                }
+            }
+        }
+
+        // 点击开关按钮
+        private void OnClickToggle_Button(object sender, RoutedEventArgs e)
+        {
+            IsOn = !IsOn;
+        }
+
+        // 点击移除按钮
+        private void OnClickToggle_Delete(object sender, RoutedEventArgs e)
+        {
+            //this.Toggle.Visibility = Visibility.Hidden;
+            if (parent != null)
+            {
+                parent.Children.Remove(this); // 移除自己
+            }   
+        }
+
+        // 文本布局更新
+        private void Toggle_Text_LayoutUpdated(object sender, EventArgs e)
+        {
+            //ActualHeight为元素的实际高度，与控件实际高度Height不同。
+            //TextBlock tmpBox = sender as TextBlock;
+            //tmpBox.Height = tmpBox.ActualHeight;
+        }
+    }
+}
